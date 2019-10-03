a90b5ac31fe4e43662954efcb06e0a4e9de5f0e0
Lee Gray <S00188181@mail.itsligo.ie>
2019-10-03 12:23:45 +0100

    Add project files.

---
+++ Q1/Cars.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Cars
+    {
+        private string make;
+
+        public string Make
+        {
+            get
+            {
+                return this.make;
+            }
+            set
+            {
+                this.make = value;
+            }
+        }
+
+        private string model;
+
+
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+
+        private double engine;
+
+        public double Engine
+        {
+            get
+            {
+                return this.engine;
+            }
+            set
+            {
+                this.engine = value;
+            }
+        }
+
+        private int speed;
+
+        public int Speed
+        {
+            get
+            {
+                return this.speed;
+
+            }
+            set
+            {
+                this.speed = value;
+            }
+        }
+    }
+}
---
+++ Q1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars c1 = new Cars();
+            c1.Make = "Toyota";
+            c1.Model = "corolla";
+            c1.Engine = 1.7;
+            c1.Speed = 0;
+
+            Cars c2 = new Cars();
+            c2.Make = "Ford";
+            c2.Model = "Focus";
+            c2.Engine = 1.1;
+            c2.Speed = 0;
+
+            Console.WriteLine($"car 1 \n Make:{c1.Make} Model:{c1.Model} Engine:{c1.Engine} Speed:{c1.Speed}\n");
+            Console.WriteLine($"car 2 \n Make:{c2.Make} Model:{c2.Model} Engine:{c2.Engine} Speed:{c2.Speed}");
+
+        }
+    }
+}
